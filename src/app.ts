4915cc60c73f4132cac723ae8702a2780b92f7fe
vikasdagur369 <vikasdagur369@gmail.com>
2024-10-02 15:25:39 +0530

    error handler added

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,13 @@
-import express, { NextFunction, Request, Response } from 'express';
-import createHttpError, { HttpError } from 'http-errors';
-import { config } from './config/config';
+import express from "express"
 import globalErrorHandler from './middlewares/globalErrorHandler';
 
 const app = express();
 
 // Routes
 app.get('/', (req, res, next) => {
-   res.json({
-    message:"welcome to elib apis"
-   }) 
+    res.json({
+        message: "welcome to elib apis"
+    })
 })
 
 
